045f343c40c7dbcc9b2c9afa23b8a78f6693f54c
Vitor George <vitor.george@gmail.com>
2021-10-29 19:16:47 -0300

    Update daily update command

--- src/br/config/index.js
+++ src/br/config/index.js
@@ -21,6 +21,7 @@ module.exports = {
   contentPath,
   countryDailyPath,
   dailyDataPath,
+  dataPath,
   emptyPbfSize: 105,
   fullHistoryFile,
   osmiumConfigs,
--- src/br/daily-update/index.js
+++ src/br/daily-update/index.js
@@ -1,3 +1,41 @@
+const fs = require("fs-extra");
+const simpleGit = require("simple-git");
+const path = require("path");
+const { dataPath } = require("../config");
+const { parseISO, addDays } = require("date-fns");
+
+const gitPath = path.join(dataPath, "git");
+const statsFile = path.join(gitPath, "stats.json");
+const initialDate = "2021-10-01Z";
+
 module.exports = async function dailyUpdate() {
-  console.log("update finished");
+  // Init repository path
+  await fs.ensureDir(gitPath);
+
+  const git = simpleGit({ baseDir: gitPath }).init();
+
+  // Get next day to update
+  let nextDay;
+  if (!(await fs.pathExists(statsFile))) {
+    nextDay = parseISO(initialDate);
+  } else {
+    const { lastUpdated } = await fs.readJSON(statsFile);
+    nextDay = addDays(parseISO(lastUpdated), 1);
+  }
+
+  // Persist last updated day
+  await fs.writeJSON(statsFile, {
+    lastUpdated: nextDay,
+  });
+
+  const nextDayISO = nextDay.toISOString();
+  await git
+    .env({
+      GIT_COMMITTER_DATE: nextDayISO,
+      GIT_AUTHOR_DATE: nextDayISO,
+    })
+    .add("./*")
+    .commit(`Status of ${nextDayISO}`);
+
+  console.log(`Updated to ${nextDayISO}`);
 };
