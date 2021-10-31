8dd5d2a620dcda8fed7cb92b862cb564c17c954c
Vitor George <vitor.george@gmail.com>
2021-10-31 14:04:30 -0300

    Turn on no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": 2,
+  },
 };
