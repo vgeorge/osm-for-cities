de730dd261d06c548c7ffbe37470909d9b754ab4
Rub21 <ruben@developmentseed.org>
2023-05-23 15:55:04 -0500

    Update script to update

--- update.sh
+++ update.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -x
 echo 'Start updating...'
-
-# yarn cli list-contexts | grep cities-of
-
 if [ -f "${TMP_DIR}/history-latest.osh.pbf" ]; then
     yarn cli update-presets-history --recursive
     yarn cli context cities-of-brazil update
