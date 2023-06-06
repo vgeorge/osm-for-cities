e2032680ad22dbc3c01b698ed718fc1764a47fcb
Rub21 <ruben@developmentseed.org>
2023-06-06 10:44:55 -0500

    Run cli in the kubernetes/docker config files

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x
-echo 'Start updating...'
-if [ -f "${TMP_DIR}/history-latest.osh.pbf" ]; then
-    yarn cli update-presets-history --recursive
-    yarn cli context cities-of-brazil update
-fi
