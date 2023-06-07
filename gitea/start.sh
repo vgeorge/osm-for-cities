23d09279e0f572d2a828ecd59105add330e63d50
Rub21 <ruben@developmentseed.org>
2023-06-07 10:17:54 -0500

    Replace values in app.ini in gitea

--- gitea/start.sh
+++ gitea/start.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
-if [ -f "$GITEA_CUSTOM/conf/app.ini" ]; then
-    rsync --ignore-existing --exclude="app.ini" -av /custom/ $GITEA_CUSTOM/
-    sed -i -e '/DISABLE_REGISTRATION/c\DISABLE_REGISTRATION = '${DISABLE_REGISTRATION}'' $GITEA_CUSTOM/conf/app.ini
-else
-    rsync -av /custom/ $GITEA_CUSTOM/
-fi
+# Write custom files
+rsync -av /custom/ $GITEA_CUSTOM/
 
-# Grant read, write permissions, TODO, change 777 
+# Grant read, write permissions, TODO: change 777
 mkdir -p /data/gitea
 chmod 777 -R /data/gitea/
 chmod 777 -R $GITEA_CUSTOM/
 
+# Replace values in $GITEA_CUSTOM/conf/app.ini
+update_app_ini() {
+    if [ -f "$GITEA_CUSTOM/conf/app.ini" ]; then
+        while IFS='=' read -r line; do
+            # Skip empty lines and comments
+            if [[ "$line" =~ ^[[:space:]]*$ ]] || [[ "$line" =~ ^[[:space:]]*# ]]; then
+                continue
+            fi
+            key=$(echo "$line" | cut -d'=' -f1)
+            value=$(echo "$line" | cut -d'=' -f2-)
+            sed -i "s/^\($key\s*=\s*\).*\$/\1$value/" "$GITEA_CUSTOM/conf/app.ini"
+        done <"/config.ini"
+    fi
+}
+
 # Start gitea
-/bin/s6-svscan /etc/s6
+update_app_ini && /bin/s6-svscan /etc/s6
