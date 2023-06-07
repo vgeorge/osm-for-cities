be669f66e26b3fc10f19ab67e86052337ae2f0fc
Rub21 <ruben@developmentseed.org>
2023-06-06 22:48:04 -0500

    Give access to dirs

--- gitea/start.sh
+++ gitea/start.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-# Grant read, write permissions
-chmod 700 /data/gitea
-chmod 700 $GITEA_CUSTOM
-
 if [ -f "$GITEA_CUSTOM/conf/app.ini" ]; then
     rsync --ignore-existing --exclude="app.ini" -av /custom/ $GITEA_CUSTOM/
     sed -i -e '/DISABLE_REGISTRATION/c\DISABLE_REGISTRATION = '${DISABLE_REGISTRATION}'' $GITEA_CUSTOM/conf/app.ini
 else
     rsync -av /custom/ $GITEA_CUSTOM/
 fi
+
+# Grant read, write permissions, TODO, change 777 
+mkdir -p /data/gitea
+chmod 777 -R /data/gitea/
+chmod 777 -R $GITEA_CUSTOM/
+
+# Start gitea
 /bin/s6-svscan /etc/s6
