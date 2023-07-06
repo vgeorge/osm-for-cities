04b3d02f363e262fd130d93f57434abaa26ff40e
Vitor George <vitor.george@gmail.com>
2023-07-06 15:45:26 +0100

    Rename helper module to avoid confusion with execa npm module

--- cli/helpers/exec.js
+++ cli/helpers/exec.js
@@ -1,23 +1,15 @@
 import execa from "execa";
 import { logger } from "./logger.js";
 
-export async function execaToStdout(cmd, args) {
+export default async function exec(cmd, args) {
   const execProcess = execa(cmd, args);
 
+  // Log stdout
   execProcess.stdout.on("data", (data) => {
     const lines = data.toString().split("\n");
     lines.forEach((line) => {
       if (line.length > 0) {
         logger.info(line);
-      }
-    });
-  });
-
-  execProcess.stderr.on("data", (data) => {
-    const lines = data.toString().split("\n");
-    lines.forEach((line) => {
-      if (line.length > 0) {
-        logger.error(line);
       }
     });
   });
